2599ad491cc2eb23446069b5b0fb1629e6562d42
Diogo Alves <diogodasilvaalves321@gmail.com>
2023-03-13 21:36:15 -0300

    exercicios typescript II concluidos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-import { users, products, purchases } from './database'
+import { users, products, purchases, createUser, getAllUsers, createProduct, getAllProducts, getProductById, queryProductByName, getAllPurchaseFromUserId } from './database'
+import { CATEGORIES } from "./types"
 
-console.table(users)
-console.table(products)
-console.table(purchases)
 
+// EXERCICIO 2 - FEEDBACK TYPESCRIPT II
+createUser("001", "teste@email.com", "senha@1234")
+getAllUsers()
+
+createProduct("004", "controle ps4", 385, CATEGORIES.GAMES)
+getAllProducts()
+getProductById("002")
+
+// EXERCICIO 3 - FEEDBACK TYPESCRIPT II
+queryProductByName("Play")
+getAllPurchaseFromUserId("001")
